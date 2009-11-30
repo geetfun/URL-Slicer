31f3cdaa061d91a2dec78fa9bb1ace904debc412
Simon Chiu <skhchiu@gmail.com>
2009-11-29 21:13:54 -0500

    added a bunch of tests

--- lib/url_slicer.rb
+++ lib/url_slicer.rb
@@ -6,6 +6,11 @@ class UrlSlicer
     @url = url
   end
   
+  def valid?
+    regex = /\A(https?|ftp|file):\/\/.+\Z/
+    @valid ||= url.scan(regex).length > 0 ? true : false
+  end
+  
   def host
     regex = /\A[a-z][a-z0-9+\-.]*:\/\/([a-z0-9\-._~%!$&'()*+,;=]+@)?([a-z0-9\-._~%]+|\[[a-z0-9\-._~%!$&'()*+,;=:]+\])/
     @host ||= url.scan(regex).flatten.compact.to_s
@@ -17,13 +22,20 @@ class UrlSlicer
   end
   
   def subdomain
+    @subdomain ||= host.gsub(domain, '').gsub('.', '')
+  end
+  
+  def port
+    regex = /^[a-z][a-z0-9+\-.]*:\/\/([a-z0-9\-._~%!$&'()*+,;=]+@)?([a-z0-9\-._~%]+|\[[a-z0-9\-._~%!$&'()*+,;=:]+\]):([0-9]+)/
+    @port ||= url.scan(regex).flatten.compact[1].to_i
     
+    @port == 0 ? default_port : @port
+  end
+  
+  def default_port
+    80
   end
   
   
-  def valid?
-    regex = /\A(https?|ftp|file):\/\/.+\Z/
-    @valid ||= url.scan(regex).length > 0 ? true : false
-  end
   
 end
--- test/test_url_slicer.rb
+++ test/test_url_slicer.rb
@@ -28,6 +28,11 @@ class TestUrlSlicer < Test::Unit::TestCase
       url = UrlSlicer.new("http://www.test.com")
       assert_equal "www.test.com", url.host
     end
+    
+    should "return www.test.com if url is http://www.test.com:80" do
+      url = UrlSlicer.new("http://www.test.com")
+      assert_equal "www.test.com", url.host
+    end
   end
   
   context "returning tld domain" do
@@ -39,6 +44,42 @@ class TestUrlSlicer < Test::Unit::TestCase
     should "return bbc.co.uk if url is http://www.bbc.co.uk" do
       url = UrlSlicer.new("http://www.bbc.co.uk")
       assert_equal "bbc.co.uk", url.domain
-    end 
+    end
+    
+    should "return test.com if url is http://www.test.com:80" do
+      url = UrlSlicer.new("http://www.test.com")
+      assert_equal "test.com", url.domain
+    end
   end
+  
+  context "returning subdomain" do
+    should "return www if url is http://www.test.com" do
+      url = UrlSlicer.new("http://www.test.com")
+      assert_equal "www", url.subdomain
+    end
+    
+    should "return blog if url is http://blog.somewhere.co.uk" do
+      url = UrlSlicer.new("http://blog.somewhere.co.uk")
+      assert_equal "blog", url.subdomain
+    end
+  end
+  
+  context "returning port" do
+    should "return port 80 if url is http://www.test.com:80" do
+      url = UrlSlicer.new("http://www.test.com:80")
+      assert_equal 80, url.port
+    end
+    
+    should "return port 8080 if url is http://www.bbc.co.uk:8080" do
+      url = UrlSlicer.new("http://www.bbc.co.uk:8080")
+      assert_equal 8080, url.port
+    end
+    
+    should "return port 80 if url is http://www.test.com" do
+      url = UrlSlicer.new("http://www.test.com")
+      assert_equal 80, url.port
+    end
+  end
+  
+  # context 
 end
